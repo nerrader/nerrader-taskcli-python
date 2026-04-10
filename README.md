4db11af0b7831346e4b2e0f768ef5db9db5c11df
lithium <darrenchang1903@gmail.com>
2026-04-10 14:19:51 +0700

    taskcli revamp thingy

--- README.md
+++ README.md
@@ -1,21 +1,8 @@
-This is a taskcli made in python by me
+**TaskCLI tool**
 
-how to install:
+I made this TaskCLI using python with the typer (and typer-extensions) library.
 
-1. download/install uv (https://astral.sh/uv)
-2. run `uv tool install git+https://github.com/nerrader/nerrader-taskcli-python`
-3. type taskcli {and another command here} to use it
+Use quotation marks to indicate multiple words
+ex: task add "go grocery shopping"
 
-commands
-
-- add (ex: taskcli add -n (--name) {name} -p (--priority: optional) {priority})
-- delete/remove/del/rm (ex: taskcli delete {id})
-- update (ex: taskcli update {id} -n (--name, updated name), -p (--priority, updated priority) (pick either name/priority or both))
-- mark (ex: taskcli mark {id} {todo/done})
-- list -p (--name, optional: filters based off priority) -s (--status, optional: filters based off status)
-- clear -y (--yes, optional: bypasses clear confirm)
-- configure/config/settings (no args)
-
-notes:
-
-- this application will store files in (your home folder/taskcli), deleting them will reset your taskcli/config (depending on what you deleted)
+Everything else can be learnt using --help
---
+++ src/taskcli/storage.py
@@ -0,0 +1,59 @@
+import json
+from pathlib import Path
+from os import makedirs, getenv
+from typing import Any
+
+# Just know that config is not implemented yet!
+MAIN_FILEPATH: Path = (
+    Path(getenv("APPDATA", Path.home() / "AppData" / "Roaming")) / "taskcli"
+)
+CONFIG_FILEPATH: Path = MAIN_FILEPATH / "config.json"
+TASKS_FILEPATH: Path = MAIN_FILEPATH / "tasks.json"
+DEFAULT_TASKS: dict[str, Any] = {"next_id": 1, "tasklist": []}
+DEFAULT_CONFIG: dict[str, Any] = {}
+
+
+def json_io(filepath: Path, data: Any = None) -> Any:
+    """handles reading and writing to files
+
+    Args:
+        filepath (Path): the filepath of the file
+        data (Any, optional): the data to write to the file, if none is provided, the function will read from the file.
+        Defaults to None.
+
+    Returns:
+        Any: the data read from the file or None if an error occurs or if it was writing
+    """
+    # reading mode
+    if data is None:
+        try:
+            with open(filepath, "r") as file:
+                file_contents = json.load(file)
+        except FileNotFoundError:
+            file_contents = None
+        return file_contents
+    # writing mode
+    else:
+        with open(filepath, "w") as file:
+            json.dump(data, file, indent=4)
+
+
+def check_storage() -> None:
+    """checks if the files exist, if not it creates them and fills them with default data"""
+
+    # make the main directory if it doesn't exist
+    makedirs(MAIN_FILEPATH, exist_ok=True)
+
+    # check if the files exist, if not create them and fill them with default data
+    if not TASKS_FILEPATH.exists():
+        json_io(TASKS_FILEPATH, DEFAULT_TASKS)
+    if not CONFIG_FILEPATH.exists():
+        json_io(CONFIG_FILEPATH, DEFAULT_CONFIG)
+
+
+def main():
+    check_storage()
+
+
+if __name__ == "__main__":
+    main()
